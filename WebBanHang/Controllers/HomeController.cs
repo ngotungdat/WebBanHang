6e1803d9d95ce703d1d582d7d167e1571f0d50f6
ngotungdat <ngotungdat1997@gmail.com>
2020-05-04 19:07:06 +0700

    Add project files.

---
+++ WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Model;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebBanHangDbContext db = new WebBanHangDbContext();
+        public ActionResult Index()
+        {
+            ViewBag.SanPhamMoi = db.SanPhams.OrderByDescending(x => x.NgayCapNhat).Take(4).ToList();
+            ViewBag.SanPhamBanChay = db.SanPhams.OrderByDescending(x => x.SoLuongBan).Take(4).ToList();
+            return View();
+        }
+
+        public ActionResult Detailes(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+    }
+}
